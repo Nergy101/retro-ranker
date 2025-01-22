c16acc2e7c9e47ea720f4f986836bdd5dd8bb191
Christian van Dijk <cdijk4@gmail.com>
2025-01-22 14:54:10 +0100

    💄 add home icons

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,4 +1,10 @@
 import { Head } from "$fresh/runtime.ts";
+import {
+  PiCalendarDots,
+  PiCalendarHeart,
+  PiRanking,
+  PiUserCheck,
+} from "@preact-icons/pi";
 import { DeviceCardSmall } from "../components/DeviceCardSmall.tsx";
 import { SeeMoreCard } from "../components/SeeMoreCard.tsx";
 import { DeviceService } from "../services/devices/device.service.ts";
@@ -38,8 +44,10 @@ export default function Home() {
         }}
       >
         {/* New Arrivals Section */}
-        <section>
-          <h2>New Arrivals</h2>
+        <section style="margin-top: 2rem;">
+          <h2 style={{ display: "flex", alignItems: "center", gap: "0.25rem" }}>
+            <PiCalendarDots /> New Arrivals
+          </h2>
           <div class="device-row-grid">
             {newArrivals.map((device) => <DeviceCardSmall device={device} />)}
             <SeeMoreCard href="/devices?sort=new-arrivals" />
@@ -48,7 +56,9 @@ export default function Home() {
 
         {/* Highly Rated Section */}
         <section style="margin-top: 2rem;">
-          <h2>Highly Rated</h2>
+          <h2 style={{ display: "flex", alignItems: "center", gap: "0.25rem" }}>
+            <PiRanking /> Highly Rated
+          </h2>
           <div class="device-row-grid">
             {highlyRated.map((device) => <DeviceCardSmall device={device} />)}
             <SeeMoreCard href="/devices?sort=highly-rated" />
@@ -57,7 +67,9 @@ export default function Home() {
 
         {/* personal Picks Section */}
         <section style="margin-top: 2rem;">
-          <h2>Personal Picks</h2>
+          <h2 style={{ display: "flex", alignItems: "center", gap: "0.25rem" }}>
+            <PiUserCheck /> Personal Picks
+          </h2>
           <div class="device-row-grid">
             {personalPicks.map((device) => <DeviceCardSmall device={device} />)}
             <SeeMoreCard href="/devices?filter=personal-picks" />
@@ -66,7 +78,9 @@ export default function Home() {
 
         {/* Upcoming Section */}
         <section style="margin-top: 2rem;">
-          <h2>Upcoming</h2>
+          <h2 style={{ display: "flex", alignItems: "center", gap: "0.25rem" }}>
+            <PiCalendarHeart /> Upcoming
+          </h2>
           <div class="device-row-grid">
             {upcoming.map((device) => <DeviceCardSmall device={device} />)}
             <SeeMoreCard href="/devices?filter=upcoming" />
