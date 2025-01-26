9ac0815cc062fd0183b16da93cb8e926519feb38
Christian van Dijk <cdijk4@gmail.com>
2025-01-26 23:25:55 +0100

    💄 change home device cards to the better medium cards

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,11 +3,12 @@ import {
   PiCalendarHeart,
   PiRanking,
   PiSparkle,
-  PiUserCheck
+  PiUserCheck,
 } from "@preact-icons/pi";
 import { DeviceCardSmall } from "../components/DeviceCardSmall.tsx";
 import { SeeMoreCard } from "../components/SeeMoreCard.tsx";
 import { DeviceService } from "../services/devices/device.service.ts";
+import { DeviceCardMedium } from "../components/DeviceCardMedium.tsx";
 
 export default function Home() {
   // Filter devices into categories
@@ -36,57 +37,89 @@ export default function Home() {
         </hgroup>
       </header>
 
-      <main
+      <div
         style={{
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
         }}
+        class="container"
       >
-        {/* New Arrivals Section */}
-        <section style="margin-top: 2rem;">
-          <h2 class="home-section-title">
-            <PiSparkle /> New Arrivals
-          </h2>
-          <div class="device-row-grid">
-            {newArrivals.map((device) => <DeviceCardSmall device={device} />)}
-            <SeeMoreCard href="/devices?sort=new-arrivals" />
-          </div>
-        </section>
+        <div className="container">
+          {/* New Arrivals Section */}
+          <section style="margin-top: 2rem;">
+            <h2 class="home-section-title">
+              <PiSparkle /> New Arrivals
+            </h2>
+            <div class="device-row-grid">
+              {newArrivals.map((device) => (
+                <DeviceCardMedium
+                  device={device}
+                  isActive={false}
+                />
+              ))}
+              <SeeMoreCard
+                href="/devices?sort=new-arrivals"
+                text="More New Arrivals"
+              />
+            </div>
+          </section>
 
-        {/* Highly Rated Section */}
-        <section style="margin-top: 2rem;">
-          <h2 class="home-section-title">
-            <PiRanking /> Highly Rated
-          </h2>
-          <div class="device-row-grid">
-            {highlyRated.map((device) => <DeviceCardSmall device={device} />)}
-            <SeeMoreCard href="/devices?sort=highly-rated" />
-          </div>
-        </section>
+          {/* Highly Rated Section */}
+          <section style="margin-top: 2rem;">
+            <h2 class="home-section-title">
+              <PiRanking /> Highly Rated
+            </h2>
+            <div class="device-row-grid">
+              {highlyRated.map((device) => (
+                <DeviceCardMedium
+                  device={device}
+                  isActive={false}
+                />
+              ))}
+              <SeeMoreCard
+                href="/devices?sort=highly-rated"
+                text="More Highly Rated"
+              />
+            </div>
+          </section>
 
-        {/* personal Picks Section */}
-        <section style="margin-top: 2rem;">
-          <h2 class="home-section-title">
-            <PiUserCheck /> Personal Picks
-          </h2>
-          <div class="device-row-grid">
-            {personalPicks.map((device) => <DeviceCardSmall device={device} />)}
-            <SeeMoreCard href="/devices?filter=personal-picks" />
-          </div>
-        </section>
+          {/* personal Picks Section */}
+          <section style="margin-top: 2rem;">
+            <h2 class="home-section-title">
+              <PiUserCheck /> Personal Picks
+            </h2>
+            <div class="device-row-grid">
+              {personalPicks.map((device) => (
+                <DeviceCardMedium
+                  device={device}
+                  isActive={false}
+                />
+              ))}
+              <SeeMoreCard
+                href="/devices?filter=personal-picks"
+                text="More Personal Picks"
+              />
+            </div>
+          </section>
 
-        {/* Upcoming Section */}
-        <section style="margin-top: 2rem;">
-          <h2 class="home-section-title">
-            <PiCalendarHeart /> Upcoming
-          </h2>
-          <div class="device-row-grid">
-            {upcoming.map((device) => <DeviceCardSmall device={device} />)}
-            <SeeMoreCard href="/devices?filter=upcoming" />
-          </div>
-        </section>
-      </main>
+          {/* Upcoming Section */}
+          <section style="margin-top: 2rem;">
+            <h2 class="home-section-title">
+              <PiCalendarHeart /> Upcoming
+            </h2>
+            <div class="device-row-grid">
+              {upcoming.map((device) => (
+                <DeviceCardMedium device={device} isActive={false} />
+              ))}
+              <SeeMoreCard
+                href="/devices?filter=upcoming"
+                text="More Upcoming"
+              />
+            </div>
+          </section>
+        </div>
+      </div>
     </div>
   );
 }
