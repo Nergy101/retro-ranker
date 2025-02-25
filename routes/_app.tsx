4dda85c1012d9ec6d3eea95b8c576d33096d4996
Christian van Dijk <cdijk4@gmail.com>
2025-02-25 19:39:22 +0100

    🔊 try to log ip of caller

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -3,8 +3,10 @@ import Footer from "../components/shared/Footer.tsx";
 import { Navbar } from "../islands/navigation/Navbar.tsx";
 import { DeviceService } from "../services/devices/device.service.ts";
 
-export default function App({ Component, url }: PageProps) {
-  console.info("App loaded");
+export default function App({ Component, url, state }: PageProps) {
+  const remoteAddr = (state.serverConnection as any)?.remoteAddr;
+  console.info("App loaded from IP:", remoteAddr ?? "unknown");
+
   const allDevices = DeviceService.getInstance().getAllDevices()
     .sort((a, b) => a.name.raw.localeCompare(b.name.raw));
 
